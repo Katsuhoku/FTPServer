b0bd19177dd647fb2e1790166e0d7358d17e35db
katsushika <marcoderio850@gmail.com>
2020-11-17 15:56:25 -0600

    Server side full commented

--- MainServer.py
+++ MainServer.py
@@ -6,27 +6,39 @@ from datetime import datetime
 from os.path import isfile, join
 from ResourceFile import ResourceFile
 
-# Clase principal. Gestiona las conexiones entrantes, y las canaliza a sus respectivos
-# hilos. Provee métodos para bloquear el acceso al sistmea de archivos, y para gestionar
-# los objetos recurso (crearlos, proveerlos y removerlos).
+# MainServer
+# Clase principal. Gestiona las conexiones entrantes y las canaliza a sus respectivos
+# hilos. Al recibir una conexión, solicita el tipo de operación, y luego el nombre
+# del archivo (excepto si la operación es List) para generar el recurso correspondiente.
+# Ejecuta en un nuevo hilo la función correspondiente del recurso a la operación
+# solicitada.
+#
+# Provee semáforos y métodos para bloquear el acceso al sistema de archivos, y para
+# obtener o generar los objetos recurso.
+#
+# Recurso: Objeto que representa y maneja el acceso a un archivo del sistema. Cada
+# archivo se asocia con un y solo un recurso. Esto permite la ejecución concurrente
+# de operaciones en diferentes recursos (salvo algunas excepciones)
 class MainServer:
     def __init__(self, port):
         self.HOST = socket.gethostname()
         self.PORT = port
-        self.activeResourceList = [] # Lista de recursos activos
-        self.listCount = 0 # Contador de hilos de listado
+        self.activeResourceList = [] # Lista de recursos arctivos
+        self.listCount = 0 # Contador de hilos de operación List
         self.countID = 0 # Threads' ID counter
 
-        self.fileSystemLock = threading.Lock() # Semáforo para bloquear el acceso al FS
+        self.fileSystemLock = threading.Lock() # Semáforo para bloquear el acceso al Sistema de Archivos
         self.listLock = threading.Lock() # Semáforo para el acceso a listCount
         self.resourceLock = threading.Lock() # Semáforo para el acceso a activeResourceList
 
-         # Gets the file list in the received files directory
+        # Obtiene la lista de archivos en el sistema.
         self.files = [f for f in listdir('./recv') if isfile(join('./recv', f))]
 
-    # Método principal    
+    # Método principal, inicia el programa
     def start(self):
-        print('Log:')
+        print('Server Log:')
+        # Creación del server socket. La cláusula 'with' maneja el socket y lo cierra
+        # automáticamente al terminar
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
             self.s.bind((self.HOST, self.PORT))
             print(f'{datetime.now()} Server started on {self.HOST}, {self.PORT}. Ready to receive connections')
@@ -43,13 +55,18 @@ class MainServer:
                     self.countID += 1
                     op = conn.recv(2).decode('utf-8', 'replace')
 
+                    # Si la operación es List, genera el nuevo hilo sin pedir más
+                    # datos
                     if op == 'ls': # List
                         threading._start_new_thread(self.listf, (conn, addr, self.countID))
                         print(f'{datetime.now()} List Thread created')
+                    # Cualquier otra operación necesita primero el nombre del archivo
+                    # deseado para generar el recurso.
                     else:
-                        # Obtains the desired file name and its resource (1)
+                        # Obtiene el nombre del archivo deseado (1)
                         name = conn.recv(1024).decode('utf-8', 'replace')
                         filename = f'recv/{name}'
+                        # Busca y obtiene el recurso asociado al archivo
                         resource = self.getResource(filename)
 
                         print(f'Working... {op}')
@@ -75,15 +92,17 @@ class MainServer:
     # El método es sincronizado consigo mismo y con removeResource
     def getResource(self, filename):
         self.resourceLock.acquire()
-        # Searchs resource
+        # Busca el recurso en la lista del servidor
         print(f'Desired: {filename}')
         for resource in self.activeResourceList:
             if resource.filename == filename:
+                # El recurso fue encontrado
                 print(f'Resource selection: {resource.filename}')
                 self.resourceLock.release()
                 return resource
         
-        # If resource doesn't exist, creates it
+        # El recurso no fue encontrado, por lo que se crea uno nuevo asociándolo al
+        # archivo solicitado
         print('Did not exist, creating resource')
         resource = ResourceFile(filename, self)
         self.activeResourceList.append(resource)
@@ -91,8 +110,8 @@ class MainServer:
         return resource
 
 
-    # Elimina el recuros que se especifica en el parámetro. El método es sincronizado
-    # consigo mismo y con getResource
+    # Elimina el recurso que se especifica en el parámetro de la lista del sistema.
+    # El método es sincronizado consigo mismo y con getResource.
     def removeResource(self, resource):
         self.resourceLock.acquire()
         try:
@@ -134,14 +153,22 @@ class MainServer:
         if self.listCount == 0: self.fileSystemLock.release()
         self.listLock.release()
 
+    # Escucha de entrada para finalizar la ejecución del servidor
+    # Se debe ejecutar en un hilo por separado, de tal forma que se mantenga escuchado
+    # una entrada de teclado cualquiera. Esto permite que el servidor pueda detener
+    # su ejecución incluso si se bloquea al esperar conexiones o datos del cliente.
     def listen_for_closing(self):
         while True:
             input()
             break
         self.s.close()
 
-
+# Main
 if __name__ == '__main__':
+    # Crea el objeto servidor con el puerto indicado en el parámetro de la ejecución
     server = MainServer(int(sys.argv[1]))
+    # Ejecuta el hilo para la finalización de la ejecución
     threading._start_new_thread(server.listen_for_closing, ())
+    # Ejecuta el servidor (no es un hilo aparte, la función se ejecuta sobre el
+    # mismo hilo actual)
     server.start()
--- ResourceFile.py
+++ ResourceFile.py
@@ -4,23 +4,48 @@ import threading
 from os.path import isfile
 from datetime import datetime
 
+# ResourceFile
 # Clase que representa un único archivo almacenado en el sistema. Provee los métodos
-# necesarios para gestioanr todos los tipos de acceso ala rchivo de forma concurrente
-# implementado el modelo de Lectores y Escritores, con prioridad a Lectores
+# necesarios para gestionar todos los tipos de acceso al archivo de forma concurrente
+# implementado el modelo de Lectores y Escritores, con prioridad a Lectores.
+#
+# Funciones Lector: download()
+# Funciones Escritor: upload(), delete()
+# Pese a ser Escritores, las operaciones upload() y delete() no son completamente
+# iguales. Eliminar un archivo del sistema no implica que el recurso también deba ser
+# eliminado. Al eliminar, otros hilos pueden estar esperando su lugar, ya sea para
+# leer (download) o para escribir (upload, delete). Es por esto que se implementan
+# otras variables comunes para dar estos avisos.
+#
+# El recurso será eliminado del sistema (al eliminar el archivo) únicamente cuando
+# no queden hilos esperando para subir (upload) información. (Si se elimina el recurso
+# podría generar inconsistencias, pues en el servidor se generaría otro recurso con
+# diferentes semáforos para los nuevos hilos después de la eliminación)
+#
+# Al eliminar, el hilo debe avisar a los demás hilos de lectura (downlaod) y escritura
+# (únicamente delete) que el archivo ha sido eliminado. Éstos deben, antes
+# de buscar el archivo en el sistema, comprobar este aviso, evitando así intentar
+# acceder a un archivo "inexistente".
 class ResourceFile:
     def __init__(self, filename, server):
-        self.filename = filename
-        self.server = server
+        self.filename = filename # Nombre del archivo asociado a este recurso
+        self.server = server # Objeto del servidor
 
-        self.readersCount = 0
-        self.uploadCount = 0
-        self.deleted = False
-        self.readersLock = threading.Lock()
-        self.uploadLock = threading.Lock()
-        self.deletedLock = threading.Lock()
-        self.writersLock = threading.Lock()
+        self.readersCount = 0 # Contador de hilos de lectura ACTIVOS (leyendo o esperando)
+        self.uploadCount = 0 # Contador de hilos de escritora upload ESPERANDO
+        self.deleted = False # Bandera de eliminación
+        self.readersLock = threading.Lock() # Semáforo para los lectores
+        self.uploadLock = threading.Lock() # Semáforo para acceder a uploadCount
+        self.deletedLock = threading.Lock() # Semáforo para acceder a la bandera deleted
+        self.writersLock = threading.Lock() # Semáforo para los escritores
 
-    # Sends file info to client
+    # Download
+    # Gestiona todo el proceso de descarga de un archivo con el cliente
+    # Consta de los siguientes pasos:
+    # (2) Confirmar la existencia del archivo en el servidor
+    # (3) Confirmar con el usuario el envío del archivo
+    # (4) Envíar el archivo
+    # (5) Esperar la confirmación del cliente de la recepción del archivo
     def download(self, conn, addr, ID):
         print(f'{datetime.now()} Thread #{ID}: Preparing for download, trying to aquire resource...')
         # Resource adquisition (I)
@@ -33,6 +58,8 @@ class ResourceFile:
 
         with conn:
             # Checking file existence (II)
+            # Primero verifica que el archivo no haya sido eliminado anteriormente por
+            # un hilo delete()
             self.deletedLock.acquire()
             if self.deleted:
                 self.deletedLock.release()
@@ -42,6 +69,7 @@ class ResourceFile:
                 self.deletedLock.release()
 
                 # File send (III)
+                # Comprueba que el archivo exista en el servidor
                 if isfile(self.filename):
                     print(f'{datetime.now()} Thread #{ID}: File found. Waiting for confirmation to send...')
                     # Reply (2)
@@ -53,7 +81,6 @@ class ResourceFile:
                     else:
                         # Sending file data (4)
                         print(f'{datetime.now()} Thread #{ID}: Download Confirmed.')
-                        # Bug here...
                         with open(self.filename, 'rb') as f:
                             data = f.read(4096)
                             print(f'{datetime.now()} Thread #{ID}: Sending...')
@@ -76,9 +103,16 @@ class ResourceFile:
         if self.readersCount == 0: self.writersLock.release()
         self.readersLock.release()
 
-    # Receives file info from client and stores it
+    # Upload
+    # Gestiona el proceso de la subida de un archivo al servidor.
+    # Consta de los siguientes pasos:
+    # (2) Confirmar la existencia del archivo en el servidor
+    # (3) Si el archivo existe, confirmar la sobreescritura
+    # (4) Recibir los datos del archivo
+    # (5) Envíar una confirmación del archivo recibido
     def upload(self, conn, addr, ID):
         # Waiting resource announcement (I)
+        # Incrementa el contador de hilos upload() en espera
         self.uploadLock.acquire()
         self.uploadCount += 1
         self.uploadLock.release()
@@ -93,19 +127,22 @@ class ResourceFile:
             # Checking file existence (III)
             exists = isfile(self.filename)
             if exists:
-                # Replu (2)
+                # Reply (2)
                 conn.send(b'y')
                 # Replace? (3)
+                # Si el archivo existe, pregunta al cliente por sobreescribir
                 replace = conn.recv(1).decode('utf-8', 'replace')
                 if replace == 'n':
                     print(f"{datetime.now()}: 302 Upload Aborted, by client in {addr}")
             else: conn.send(b'n') # Reply (2)
 
             # Data receving (IV)
+            # Si el archivo no existía, o existía y se confirmó la sobreescritura
             if exists and replace == 'y' or not exists:
                 with open(self.filename, 'wb') as f:
                     conn.settimeout(5)
                     try:
+                        # Recibe los datos del archivo (4)
                         data = conn.recv(4096)
                         while True:
                             f.write(data)
@@ -118,6 +155,7 @@ class ResourceFile:
                 print(f"{datetime.now()}: 100 Upload Successfull, stored {self.filename} from client in {addr}")
 
                 # File list update (V)
+                # Actualiza la lista de archivos en el servidor
                 self.server.updateFileList()
 
         # Resource liberation (VI)
@@ -126,8 +164,13 @@ class ResourceFile:
         self.deletedLock.release()
         self.writersLock.release()
     
-    # Removes a file from server
-    # If there's no threads waiting in upload(), also removes this resource from server list
+    # Remove
+    # Gestiona el proceso de la eliminación de un archivo almacenado en el servidor
+    # Consta de los siguientes pasos:
+    # (2) Confirmar la existencia del archivo
+    # (3) Si el archivo existe, confirmar con el usuario la eliminación (esta operación
+    # no se puede revertir)
+    # (4) Eliminar el archivo y confirmar al usuario la eliminación
     def delete(self, conn, addr, ID):
         # Resource Adquisition (I)
         self.writersLock.acquire()
@@ -136,6 +179,7 @@ class ResourceFile:
             # File doesn't more exist (II)
             self.deletedLock.acquire()
             # If file was already deleted while wating, ends process
+            # Si el archivo fue eliminado por un hilo delete() anterior, finaliza
             if self.deleted:
                 self.deletedLock.release()
                 conn.send(b'n') # Reply (2)
@@ -149,6 +193,7 @@ class ResourceFile:
                     # Reply (2)
                     conn.send(b'y')
                     # Remove? (3)
+                    # Pregunta al usuario por última vez si desea eliminar el archivo
                     remove = conn.recv(1).decode('utf-8', 'replace')
                     if remove == 'n':
                         print(f"{datetime.now()}: 303 Delete Aborted, by client in {addr}")
@@ -160,13 +205,14 @@ class ResourceFile:
                         conn.send(b'100')
 
                         # Removing resource from server (V)
-                        # Prevents Upload threads to enter while is checking for already wating Uplaod threads
-                        # in this resource
+                        # Elimina este recurso de la lista del servidor, solo si no
+                        # quedan hilos upload() esperando.
                         self.uploadLock.acquire()
                         if self.uploadCount == 0: self.server.removeResource(self)
                         self.uploadLock.release()
 
                         # File list update (VI)
+                        # Actualiza la lista de archivos en el servidor
                         self.server.updateFileList()
                         print(f"{datetime.now()}: 100 Delete Successfull, {self.filename} was succesfully deleted")
                 else:
